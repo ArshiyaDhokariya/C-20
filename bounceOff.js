fdd1374eb6a333ecbe39168984961632edf9c67a
ArshiyaDhokariya <73935173+ArshiyaDhokariya@users.noreply.github.com>
2020-11-13 13:10:00 +0530

    Add files via upload

---
+++ bounceOff.js
@@ -0,0 +1,37 @@
+var movingRect ;
+var fixedRect ;
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(650, 200, 50, 100);
+  movingRect = createSprite(100,200,60,35);
+  
+   movingRect.velocityX = 7;
+   fixedRect.velocityX = -7;
+
+ movingRect.shapeColor = "green";
+ fixedRect.shapeColor = "green";
+
+}
+ function draw() {
+    background(255,255,255);
+  
+   // movingRect.x = mouseX;
+    //movingRect.y = mouseY;
+  
+    if(movingRect.x - fixedRect.x < movingRect.width/2 + fixedRect.width/2  &&  
+      fixedRect.x - movingRect.x < fixedRect.width/2+movingRect.width/2 )
+     
+      {
+         movingRect.velocityX = movingRect.velocityX * - 1
+         fixedRect.velocityX = fixedRect.velocityX * - 1
+      }
+      if ( movingRect.y - fixedRect.y < movingRect.height/2 + fixedRect.height/2  &&  
+        fixedRect.y - movingRect.y < fixedRect.height/2+movingRect.height/2 )
+        {
+            movingRect.velocityY = movingRect.velocityY * - 1
+            fixedRect.velocityY= fixedRect.velocityY * - 1
+        }
+    
+      drawSprites();
+    }
